b347ebb4c2bf2fd6fac6b456875dd0c1805a6eb3
Juan Eiros <jeirosz@gmail.com>
2025-03-19 17:11:38 +0100

    missing inputs arg?
    how does this shit work

--- configuration.nix
+++ configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ inputs, config, pkgs, ... }:
 
 {
   imports =
@@ -28,7 +28,7 @@
     trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
   };
 
-    programs.hyprland = {
+  programs.hyprland = {
     enable = true;
     # set the flake package
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
