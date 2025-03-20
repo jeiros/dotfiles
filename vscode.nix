fa8c51dea005d86d9419ade8972142a03c938adf
Juan Eiros Zamora <jeirosz@gmail.com>
2025-03-20 10:02:43 +0100

    fix vscode?

--- vscode.nix
+++ vscode.nix
@@ -1,5 +1,15 @@
 {
-  inputs.vscode-server.url = "github:nix-community/nixos-vscode-server";
+
+description = "VScode config flake";
+
+inputs = {
+    vscode-server.url = "github:nix-community/nixos-vscode-server";
+
+    # home-manager = {
+    #   url = "github:nix-community/home-manager";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+  };
 
   outputs = { self, nixpkgs, vscode-server }: {
     nixosConfigurations.yourhostname = nixpkgs.lib.nixosSystem {
