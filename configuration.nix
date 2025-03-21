99c1489b29719ab2e9134cc48aad9aac7d945c7e
Juan Eiros Zamora <jeirosz@gmail.com>
2025-03-21 16:06:41 +0100

    fix services.console

--- configuration.nix
+++ configuration.nix
@@ -48,70 +48,66 @@
   };
 
   # Configure X11
-
-  services = {
-    xserver = {
-      enable = true;
-      xkb = {
-        layout = "es";
-        variant = "nodeadkeys";
-      };
-      displayManager.defaultSession = "none+i3";
+  services.xserver = {
+    enable = true;
+    xkb = {
+      layout = "es";
+      variant = "nodeadkeys";
     };
-    # Configure console keymap
-    console.keyMap = "uk";
-
-    # Define a user account. Don't forget to set a password with 'passwd'.
-    users.users.juan = {
-      isNormalUser = true;
-      description = "Juan";
-      extraGroups = [ "networkmanager" "wheel" ];
-      packages = with pkgs; [];
-    };
-
-    # Allow unfree packages
-    nixpkgs.config.allowUnfree = true;
-
-    # List packages installed in system profile. To search, run:
-    # $ nix search wget
-    environment.systemPackages = with pkgs; [
-      vim 
-      neovim
-      wget
-      git
-      google-chrome
-      rofi            # Better application launcher
-      feh             # Image viewer, used for setting wallpapers
-      picom           # Compositor for transparency
-      alacritty       # A modern terminal
-      arandr          # GUI for xrandr (display management)
-      xorg.xbacklight # Backlight control
-      lxappearance    # Theme switcher
-    ];
-
-    # Explicitly disable PipeWire
-    services.pipewire.enable = false;
-    services.pulseaudio.enable = true;
-
-    # List services that you want to enable:
-
-    # Enable the OpenSSH daemon.
-    services.openssh.enable = true;
-
-    # Open ports in the firewall.
-    # networking.firewall.allowedTCPPorts = [ ... ];
-    # networking.firewall.allowedUDPPorts = [ ... ];
-    # Or disable the firewall altogether.
-    networking.firewall.enable = false;
-
-    # This value determines the NixOS release from which the default
-    # settings for stateful data, like file locations and database versions
-    # on your system were taken. It's perfectly fine and recommended to leave
-    # this value at the release version of the first install of this system.
-    # Before changing this value read the documentation for this option
-    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-    system.stateVersion = "24.11"; # Did you read the comment?
+    displayManager.defaultSession = "none+i3";
   };
+  
+  # Configure console keymap
+  console.keyMap = "uk";
+
+  # Define a user account. Don't forget to set a password with 'passwd'.
+  users.users.juan = {
+    isNormalUser = true;
+    description = "Juan";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim 
+    neovim
+    wget
+    git
+    google-chrome
+    rofi            # Better application launcher
+    feh             # Image viewer, used for setting wallpapers
+    picom           # Compositor for transparency
+    alacritty       # A modern terminal
+    arandr          # GUI for xrandr (display management)
+    xorg.xbacklight # Backlight control
+    lxappearance    # Theme switcher
+  ];
+
+  # Explicitly disable PipeWire
+  services.pipewire.enable = false;
+  services.pulseaudio.enable = true;
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
 
   home-manager = {
     useGlobalPkgs = true;
