b82dde9688c0e59ac08b0784d8c2fba92c215e6e
Juan Eiros Zamora <jeirosz@gmail.com>
2025-03-21 16:10:01 +0100

    redo config

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,6 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running 'nixos-help').
+# and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { inputs, config, pkgs, ... }:
 
@@ -48,19 +48,31 @@
   };
 
   # Configure X11
-  services.xserver = {
-    enable = true;
-    xkb = {
-      layout = "es";
-      variant = "nodeadkeys";
+
+  services = {
+    xserver = {
+      enable = true;
+      windowManager.i3 = {
+        enable = true;
+        package = pkgs.i3;
+        extraPackages = with pkgs; [
+          dmenu          # Application launcher
+          i3status       # Status bar
+          i3lock         # Screen locker
+          i3blocks       # Status bar alternative
+        ];
+      };
+      xkb = {
+        layout = "es";
+        variant = "nodeadkeys";
+      };
     };
     displayManager.defaultSession = "none+i3";
   };
-  
   # Configure console keymap
   console.keyMap = "uk";
 
-  # Define a user account. Don't forget to set a password with 'passwd'.
+  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.juan = {
     isNormalUser = true;
     description = "Juan";
@@ -92,6 +104,16 @@
   services.pipewire.enable = false;
   services.pulseaudio.enable = true;
 
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
@@ -103,7 +125,7 @@
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
+  # on your system were taken. It‘s perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
